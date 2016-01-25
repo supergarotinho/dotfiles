6b2c8cfd7551a01a542cf4894d57b2c38f0da4ae
Igor1201 <igor@borges.me>
2016-01-25 12:08:43 -0200

    Added casks

--- igor.sh
+++ igor.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-cd
-mkdir Projects
+mkdir ~/Projects
 
 git config --global user.email "igor@borges.me"
 git config --global user.name "Igor1201"
 git config --global user.signingkey "7AD24624"
 
 curl https://install.meteor.com/ | sh
+
+brew cask install google-chrome
+brew cask install telegram
+brew cask install spectacle
+brew cask install xbox360-controller-driver
+brew cask install utorrent
+brew cask install android-studio
