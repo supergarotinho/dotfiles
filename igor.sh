edfa60091ea366613cae9678ffe2f6a61b645ced
Igor Borges <igor@borges.me>
2016-08-29 20:30:04 -0300

    zprofile, small igor.sh changes

--- igor.sh
+++ igor.sh
@@ -2,16 +2,13 @@
 mkdir ~/Projects
 
 git config --global user.email "igor@borges.me"
-git config --global user.name "Igor1201"
+git config --global user.name "Igor Borges"
 git config --global user.signingkey "7AD24624"
 git config --global core.excludesfile "~/.gitignore"
 
-curl https://install.meteor.com/ | sh
+#curl https://install.meteor.com/ | sh
 
 brew cask install iterm2
-brew cask install google-chrome
-brew cask install telegram
 brew cask install spectacle
 brew cask install xbox360-controller-driver
-brew cask install utorrent
-brew cask install android-studio
+#brew cask install android-studio
