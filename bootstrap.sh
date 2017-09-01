14e5e02fa7107ea8b9e359500e2e81727fcdea0f
Igor Borges <igor@borges.me>
2017-09-01 00:26:29 -0300

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,12 +24,12 @@ function doIt() {
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
 
-	customDoIt
+	customDoIt;
 
 	source ~/.zprofile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
@@ -39,3 +39,4 @@ else
 	fi;
 fi;
 unset doIt;
+unset customDoIt;
